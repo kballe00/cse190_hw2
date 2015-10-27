2c9eb1c6217c0b742cf37644cbddec9cd389f528
Keith Ballew <kballe00@gmail.com>
2015-10-27 08:08:37 -0700

    "Created randomlyInitWeights.m for randomly creating weight matrices."

---
+++ randomlyInitWeights.m
@@ -0,0 +1,18 @@
+function weights = randomlyInitWeights( numInConnections, numOutConnections )
+%RANDOMLYINITWEIGHTS Randomly initialize the weights of a layer with
+%numInConnections incoming connections and numOutCounnections outgoing connections
+%   weights = RANDOMLYINITWEIGHTS(numInConnections, numOutConnections) randomly
+%   initializes the weights of a layer with numInConncections incoming connections
+%   and numOutConnections outgoing connections. 
+%
+%   Note that weights should be set to a matrix of size( numInConnections,
+%   numOutConnections + 1 ) as the column row of weights handles the "bias" terms
+%
+
+% Randomly initialize the weights to small values
+epsilon = 0.10;
+weights = rand( numInConnections, numOutConnections + 1 ) * 2 * ...
+    epsilon - epsilon;
+
+end
+
