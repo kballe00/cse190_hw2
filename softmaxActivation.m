8fa8efcbb9e6c76cda2542973b3c51da5ab51c54
Keith Ballew <kballe00@gmail.com>
2015-10-28 09:49:31 -0700

    "Changed softmaxActivation.m to work better with current assignment."

--- softmaxActivation.m
+++ softmaxActivation.m
@@ -1,10 +1,10 @@
-function [ probabilityVector ] = softmaxActivation( weights, trainingData )
+function [ probabilityVector ] = softmaxActivation( layerInput )
 %softmaxActiviation This function computes the softmax probabilites given
 %                   given a weight matrix/vector weights and a set of
 %                   training data trainingData    
     
-    probabilityVector = exp( weights.' * trainingData.' ) / sum( exp( ...
-        weights.' * trainingData.' ) );        
+    probabilityVector = exp( layerInput ) / sum( exp( ...
+        layerInput ) );        
 
 end
 
