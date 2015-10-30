6c485168275567c21fc66fcfb05c1cd507555332
Keith Ballew <kballe00@gmail.com>
2015-10-30 11:57:27 -0700

    "Altered the way softmaxActivation.m handles input matrices."

--- softmaxActivation.m
+++ softmaxActivation.m
@@ -1,10 +1,10 @@
 function [ probabilityVector ] = softmaxActivation( layerInput )
 %softmaxActiviation This function computes the softmax probabilites given
 %                   given a weight matrix/vector weights and a set of
-%                   training data trainingData    
-    
-    probabilityVector = exp( layerInput ) / sum( exp( ...
-        layerInput ) );        
+%                   training data trainingData   
+
+    probabilityVector = bsxfun( @rdivide, exp( layerInput ), sum( exp( ...
+        layerInput ), 2 ) );        
 
 end
 
