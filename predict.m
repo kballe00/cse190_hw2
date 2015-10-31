40413c2adeedd2d338579a8755d1012df612aa25
Keith Ballew <kballe00@gmail.com>
2015-10-31 12:44:05 -0700

    "Created predict.m to take trained weights and input data, and then output the predicted class label for each training/testing case."

---
+++ predict.m
@@ -0,0 +1,19 @@
+function predictions = predict( firstLayerWeights, secondLayerWeights, ...
+    trainingData )
+%PREDICT Predict the label of an input given a trained neural network
+%   predictions = PREDICT( firstLayerWeights, secondLayerWeights, trainingData )
+%   outputs the predicted labels of trainingData given the trained weights
+%   firstLayerWeights and secondLayerWeights.
+
+% Initialize variables
+numTrainingCases = size( trainingData, 1 );
+
+% Add bias to training data
+trainingData = [ ones( numTrainingCases, 1 ) trainingData ];
+hiddenLayerOutput = sigmoid( trainingData * firstLayerWeights.' );
+% Add bias to hidden layer output
+hiddenLayerOutput = [ ones( trainingData, 1 ) hiddenLayerOutput ];
+output = softmaxActivation( hiddenLayerOutput * secondLayerWeights.' );
+
+[ ~, predictions ] = max( output, [ ], 2 );
+end
