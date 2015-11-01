336934e6c52b6b9e08f0556cb93a672d672d7618
Keith Ballew <kballe00@gmail.com>
2015-11-01 13:17:49 -0800

    "Added code to cse190HW2Driver.m to implement stochastic gradient descent. Also, adjusted some parameters to optimize learning."

--- cse190HW2Driver.m
+++ cse190HW2Driver.m
@@ -1,47 +1,59 @@
 % Read in data
 [ images, labels ] = read_mnist_training_files( '~/Downloads/train-images-idx3-ubyte', ...
-    '~/Downloads/train-labels-idx1-ubyte', 20000, 0 );
+    '~/Downloads/train-labels-idx1-ubyte', 60000, 0 );
 
 % Initialize variables
-numTrainingCases = 20000;
+numTrainingCases = 50000;
+numTestCases = 10000;
 numberOfFeatures = 784;
 hiddenLayerSize = 25;
 outputLayerSize = 10;
-numberOfEpochs = 50;
-regularizationTerm = 100;
-learningRate = 1;
+numberOfEpochs = 5;
+regularizationTerm = 0;
+learningRate = 0.1;
 
 % Map 0's to 10's for easier indexing later
 labels( labels == 0 ) = 10;
 
 % Transform single vector of data into a matrix representation of the data
 trainingCases = zeros( numTrainingCases, numberOfFeatures );
+testCases = zeros( numTestCases, numberOfFeatures );
 for i = 1 : numTrainingCases
-   trainingCases( i, : ) = reshape( images( :, :, i ).', numberOfFeatures, 1);
+    trainingCases( i, : ) = reshape( images( :, :, i ).', numberOfFeatures, 1);
+end
+for i = 1 + numTrainingCases : numTestCases
+    testCases( i, : ) = reshape( images( :, :, i ).', numberOfFeatures, 1);
 end
 
 firstLayerWeights = randomlyInitWeights( numberOfFeatures, hiddenLayerSize );
 secondLayerWeights = randomlyInitWeights( hiddenLayerSize, outputLayerSize );
 weightsAsVector = [ firstLayerWeights( : ); secondLayerWeights( : ) ];
 
-% Train neural net using batch processing
+% Train neural net using stochstic gradient descent
 for i = 1 : numberOfEpochs
-    [ cost, gradient ] = neuralNetCostFunc( weightsAsVector, ...
-        numberOfFeatures, hiddenLayerSize, outputLayerSize, trainingCases, ...
-        labels, regularizationTerm );
+    % Randomize the order in which training cases are processed
+    testCaseIndices = randperm( numTrainingCases );
     
-    firstLayerGradient = reshape( gradient( 1 : hiddenLayerSize * ( numberOfFeatures ...
-        + 1 ) ), hiddenLayerSize, ( numberOfFeatures + 1 ) );
-    secondLayerGradient = reshape( gradient( ( 1 + ( hiddenLayerSize * ( numberOfFeatures ...
-        + 1 ) ) ) : end ), outputLayerSize, ( hiddenLayerSize + 1 ) );
-    
-    firstLayerWeights = firstLayerWeights - learningRate * firstLayerGradient;
-    secondLayerWeights = secondLayerWeights - learningRate * secondLayerGradient;
-    
-    weightsAsVector = [ firstLayerWeights( : ); secondLayerWeights( : ) ];
+    for j = 1 : numTrainingCases
+        index = testCaseIndices( 1, j );
+        
+        [ cost, gradient ] = neuralNetCostFunc( weightsAsVector, ...
+            numberOfFeatures, hiddenLayerSize, outputLayerSize, ... 
+            trainingCases( index, : ), labels( index ), regularizationTerm );
+
+        firstLayerGradient = reshape( gradient( 1 : hiddenLayerSize * ( numberOfFeatures ...
+            + 1 ) ), hiddenLayerSize, ( numberOfFeatures + 1 ) );
+        secondLayerGradient = reshape( gradient( ( 1 + ( hiddenLayerSize * ( numberOfFeatures ...
+            + 1 ) ) ) : end ), outputLayerSize, ( hiddenLayerSize + 1 ) );
+
+        firstLayerWeights = firstLayerWeights - learningRate * firstLayerGradient;
+        secondLayerWeights = secondLayerWeights - learningRate * secondLayerGradient;
+
+        weightsAsVector = [ firstLayerWeights( : ); secondLayerWeights( : ) ];
+    end
 end
 
 % Test neural net accuracy
-predictions = predict( firstLayerWeights, secondLayerWeights, trainingCases );
+predictions = predict( firstLayerWeights, secondLayerWeights, testCases );
 fprintf('\nTraining set accuracy: %f\n', mean( double( predictions == labels ) ) ...
     * 100 );
